1aa73d4d5bba341100dbf8d095245fb3a4e996d3
Rachel Chan <r.achiechan@gmail.com>
2019-11-28 16:42:47 -0500

    express api to change error message

--- routes/api/mmr.js
+++ routes/api/mmr.js
@@ -12,7 +12,14 @@ router.post("/test", function(req, res) {
       `https://${req.body.server}.whatismymmr.com/api/v1/summoner?name=${req.body.summonerName}`
     )
     .then(response => {
+      // console.log(response);
       res.send(response.data[req.body.gameMode]);
+    })
+    .catch(function(error) {
+      console.log("no MMR data for summoner");
+      res.status(404).send("no MMR data for summoner");
+      // res.send(error.response.data.error)
+      // res.send({message: "no MMR data for summoner"})
     });
 });
 
